ae09e53d4ca540c689b9ab9dcfb9eed5feb3440e
Vijay <Visush@Visushs-Mac-Pro.local>
2013-02-02 16:45:25 +0530

    Initial Commit

---
+++ VjButton Demo/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  VjButton Demo
+//
+//  Created by Vijay on 02/02/13.
+//  Copyright (c) 2013 vj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ VjButton Demo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  VjButton Demo
+//
+//  Created by Vijay on 02/02/13.
+//  Copyright (c) 2013 vj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
